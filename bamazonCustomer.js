7bc93ad5c00bafd510fc57d1d395470f4674ec34
Youngju Lee <lucylee839@hotmail.com>
2019-05-12 16:46:33 -0700

    add product sales function to bamazon customer and created departments table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,6 +31,7 @@ var customer = {
     salesItem: function (list) {
         var space = " ";
         var spaceTwo = " ";
+        console.log("Welcome to Bamazon!!!");
         for (var i in list) {
             for (var j = 5; j >= list[i].item_id.toString().length; j--) {
                 space += " ";
@@ -38,7 +39,6 @@ var customer = {
             for (var k = 20; k >= list[i].product_name.length; k--) {
                 spaceTwo += " ";
             }
-            console.log("Welcome to Bamazon!!!");
             console.log("ITEM ID : " + list[i].item_id + space + " PRODUCT NAME : " + list[i].product_name +
                 spaceTwo + " PRICE : $" + list[i].price);
             var space = " ";
@@ -71,57 +71,72 @@ var customer = {
             .then(function (response) {
                 var itemNum = response.item;
                 var itemQuantity = response.quantity;
- 
+
                 customer.checkInStock(itemNum, itemQuantity);
 
             });
     },
-    checkInStock : function (itemNum, itemQuantity){
+    checkInStock: function (itemNum, itemQuantity) {
         var query = "SELECT * FROM products"
         connection.query(query, function (err, res) {
             if (err) throw err;
             var currentStock = res[itemNum - 1].stock_quantity;
-    
+
             if (currentStock >= itemQuantity) {
                 customer.update(itemNum, itemQuantity, currentStock);
             } else {
                 console.log("Sorry, Not enough in stock!");
                 customer.promptContinue();
             }
-    
+
         });
     },
-    update :function (itemNum, itemQuantity, currentStock){
-      
-            console.log("processing the order...");
-            var updateStock = currentStock - itemQuantity;
-            var query = connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: updateStock
-                    },
-                    {
-                        item_id: itemNum
-                    }
-                ],
-                function (err, res) {
-                    customer.report(itemNum, itemQuantity);
+    update: function (itemNum, itemQuantity, currentStock) {
+
+        console.log("processing the order...");
+        var updateStock = currentStock - itemQuantity;
+        var query = connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: updateStock
+                },
+                {
+                    item_id: itemNum
                 }
-            );
-    
+            ],
+            function (err, res) {
+                customer.report(itemNum, itemQuantity);
+            }
+        );
+
     },
-    report : function (itemNum, itemQuantity){
+    report: function (itemNum, itemQuantity) {
         var query = "SELECT * FROM products"
         connection.query(query, function (err, list) {
             // console.log(res);
-            console.log("You are purchasing " + itemQuantity + " " +
-                list[itemNum - 1].product_name + "...TOTAL COST IS $" + (list[itemNum - 1].price * itemQuantity));
-            customer.promptContinue();
-        });
+            var total = list[itemNum - 1].price * itemQuantity;
+        console.log("You are purchasing " + itemQuantity + " " +
+            list[itemNum - 1].product_name + "...TOTAL COST IS $" + total);
+        var productSales = list[itemNum - 1].product_sales + total;
+        var query = connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    product_sales: productSales
+                },
+                {
+                    item_id: itemNum
+                }
+            ],
+            function (err, res) {
+                customer.promptContinue();
+            }
+        );
+    });
     },
-    promptContinue : function(){
-        inquirer
+promptContinue: function () {
+    inquirer
         .prompt({
             name: "again",
             type: "confirm",
@@ -135,7 +150,7 @@ var customer = {
                 connection.end();
             }
         });
-    }
+}
 
 
 }
