ffc8591cb7a19e732cc30e906b9c49e3f6b55185
Youngju Lee <lucylee839@hotmail.com>
2019-05-11 17:31:21 -0700

    full customer app loaded

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,16 +15,15 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    loadData();
+    customer.loadData();
 });
 
-function loadData() {
-    var query = "SELECT * FROM products"
-}
 
-var cusotmer = {
+
+var customer = {
     loadData: function () {
-        connection.query(query, function (err, res) {
+        var query = "SELECT * FROM products"
+        connection.query(query, function (err, list) {
             if (err) throw err;
             customer.salesItem(list)
         });
@@ -71,80 +70,57 @@ var cusotmer = {
             .then(function (response) {
                 var itemNum = response.item;
                 var itemQuantity = response.quantity;
-                //   console.log(res.item);
-                //   console.log(res.quantity);
+ 
                 customer.checkInStock(itemNum, itemQuantity);
-                //   result(itemNum)
+
             });
     },
     checkInStock : function (itemNum, itemQuantity){
         var query = "SELECT * FROM products"
         connection.query(query, function (err, res) {
             if (err) throw err;
-            // console.log(res);
-            // console.log(res[itemNum-1].stock_quantity);
             var currentStock = res[itemNum - 1].stock_quantity;
     
             if (currentStock >= itemQuantity) {
-                result(itemNum, itemQuantity, currentStock);
+                customer.update(itemNum, itemQuantity, currentStock);
             } else {
                 console.log("Sorry, Not enough in stock!");
-                promptContinue();
+                customer.promptContinue();
             }
     
-            // promptUser(res)
-            // connection.end();
         });
-    }
-
-
-}
-
-function result(itemNum, itemQuantity, currentStock) {
-    var query = "SELECT * FROM products"
-    connection.query(query, function (err, res) {
-        if (err) throw err;
-        // console.log(res);
-        console.log("processing the order...");
-        var updateStock = currentStock - itemQuantity;
-        var query = connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-                {
-                    stock_quantity: updateStock
-                },
-                {
-                    item_id: itemNum
+    },
+    update :function (itemNum, itemQuantity, currentStock){
+      
+            console.log("processing the order...");
+            var updateStock = currentStock - itemQuantity;
+            var query = connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: updateStock
+                    },
+                    {
+                        item_id: itemNum
+                    }
+                ],
+                function (err, res) {
+                    customer.report(itemNum, itemQuantity);
                 }
-            ],
-            function (err, res) {
-                // console.log(res);
-                // if (err){
-                //     console.log(err);
-                // }
-                report(itemNum, itemQuantity);
-            }
-        );
-        // Cconsole.log(res[itemNum-1].product_name);
-        // promptUser(res)
-        // connection.end();
-
-    });
-
-}
-
-function report(itemNum, itemQuantity) {
-    var query = "SELECT * FROM products"
-    connection.query(query, function (err, res) {
-        // console.log(res);
-        console.log("You are purchasing " + itemQuantity + " " +
-            res[itemNum - 1].product_name + "...TOTAL COST IS $" + (res[itemNum - 1].price * itemQuantity));
-        promptContinue();
-    });
-
-}
-function promptContinue() {
-    inquirer
+            );
+    
+    },
+    report : function (itemNum, itemQuantity){
+        var query = "SELECT * FROM products"
+        connection.query(query, function (err, list) {
+            // console.log(res);
+            console.log("You are purchasing " + itemQuantity + " " +
+                list[itemNum - 1].product_name + "...TOTAL COST IS $" + (list[itemNum - 1].price * itemQuantity));
+            customer.promptContinue();
+        });
+    },
+    promptContinue : function(){
+        inquirer
         .prompt({
             name: "again",
             type: "confirm",
@@ -152,10 +128,14 @@ function promptContinue() {
         })
         .then(function (answer) {
             if (answer.again === true) {
-                loadData();
+                customer.loadData();
             } else {
                 console.log("Come back again soon!");
                 connection.end();
             }
         });
+    }
+
+
 }
+
