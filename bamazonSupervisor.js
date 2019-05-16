f31b9d28cf81fa2c0dd6bd3797f55786f4c0fa24
Youngju Lee <lucylee839@hotmail.com>
2019-05-15 21:17:48 -0700

    cleaned up codes on bamazonSupervisor.js and added comments

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,9 +1,13 @@
+// packages required
 require("dotenv").config();
 var mysql = require("mysql");
 var keys = require("./keys.js");
 var inquirer = require("inquirer");
 
+// for keeping password private
 var myPassword = keys.keys.password;
+
+// mysql connection 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,12 +16,17 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
+// check the connection
 connection.connect(function (err) {
     if (err) throw err;
     supervisor.promptSupervisor();
 });
 
+// rowCount for view 
+const rowCount = 0; 
+
 var supervisor = {
+    // function that prompt supervisor to choose from the options and call function accordingly
     promptSupervisor: function () {
         inquirer
             .prompt([
@@ -34,22 +43,7 @@ var supervisor = {
                     var command = res.menu;
                     switch (command) {
                         case ("View Product Sales by Department"):
-                            // supervisor.view(list);
-                            console.log("| Department_Id |Department_Name|Over_Head_Costs" +
-                                "| Product_Sales | total_Profit  |");
-                            var line = "";
-                            for (var i = 0; i < 5; i++) {
-                                line += "|";
-                                for (var j = 0; j < 15; j++) {
-                                    line += "-";
-                                }
-                            }
-                            line += "|";
-                            console.log(line);
-                            for (var i in list) {
-                                supervisor.getProductSales(list[i]);
-                            }
-                            
+                            supervisor.getProductSales(list);
                             break;
                         case ("Create New Department"):
                             supervisor.create(list);
@@ -57,48 +51,37 @@ var supervisor = {
                     }
                 });
             });
-
     },
-    view: function (list, productSales) {
-        // console.log(list);
-        var row = "";
 
-
-        // for (var i in list) {
-        row += "|" + list.department_id;
-        for (var j = 15; j > list.department_id.toString().length; j--) {
-            row += " ";
+    // function that gets space and return it, so that all rows and columns are lined up
+    getSpace: function (length) {
+        var space = " ";
+        for (var i = 13; i >= length; i--) {
+            space += " ";
         }
-        row += "|" + list.department_name;
-        for (var j = 15; j > list.department_name.length; j--) {
-            row += " ";
-        }
-        row += "|" + list.over_head_costs;
-        for (var j = 15; j > list.over_head_costs.toString().length; j--) {
-            row += " ";
-        }
-        row += "|" + productSales;
-        for (var j = 15; j > productSales.toString().length; j--) {
-            row += " ";
-        }
-        var profit = productSales - list.over_head_costs;
-        row += "|" + profit;
-        for (var j = 15; j > profit.toString().length; j--) {
-            row += " ";
-        }
-
-
-        console.log(row + "|");
-
-        row = "";
- 
-
+        return space;
     },
+
+    // function that starts the table and call view function to create each row 
     getProductSales: function (list) {
-        // console.log(list);
+        console.log("| Department_Id |Department_Name|Over_Head_Costs" +
+            "| Product_Sales | total_Profit  |");
+        var line = "";
+        for (var i = 0; i < 5; i++) {
+            line += "|";
+            for (var j = 0; j < 15; j++) {
+                line += "-";
+            }
+        }
+        line += "|";
+        console.log(line);
+        for (var i in list) {
+            supervisor.view(list[i], list.length);
+        }
+    },
 
-
-        // for (var i in list) {
+    // function that creates each row by deparmtment name
+    view: function (list, listLength) {
         connection.query(
             "SELECT * FROM products WHERE ?",
             {
@@ -107,22 +90,32 @@ var supervisor = {
             function (err, res) {
                 var productSales = 0;
                 for (var k in res) {
-
-                    // console.log(res[k].product_sales);
                     if (res[k].product_sales != null) {
                         productSales += res[k].product_sales;
                     }
-
                 }
-                // console.log(productSales);
-                supervisor.view(list, productSales);
-          
+    
+                var row = "|" + list.department_id + supervisor.getSpace(list.department_id.toString().length) +
+                    "|" + list.department_name + supervisor.getSpace(list.department_name.length) + "|" +
+                    list.over_head_costs + supervisor.getSpace(list.over_head_costs.toString().length) + "|" +
+                    productSales + supervisor.getSpace(productSales.toString().length);
 
-            }
+                var profit = productSales - list.over_head_costs;
 
-        );
-
+                row += "|" + profit + supervisor.getSpace(profit.toString().length);
+                console.log(row + "|");
+                rowCount++;
+                row = "";
+                
+                // if all rows are created, call promptContinue function to ask 
+                // if supervisor wants to do another job
+                if (rowCount == listLength){
+                    supervisor.promptContinue();
+                }
+            });
     },
+
+    // function to create new department
     create: function (list) {
         inquirer
             .prompt([
@@ -142,30 +135,34 @@ var supervisor = {
                 },
             ])
             .then(function (response) {
-                // if it is already on the list, let user know to use other option.
-                // var query = "SELECT * FROM departments"
-                // connection.query(query, function (err, list) {
+                 // make sure the depatment is not already existing
+                var inStock = false;
                 for (var i in list) {
-                    if (response.name == list[i].department_name) {
-                        console.log("deparmtent is already in stock. Please use Add to Inventory option to add");
-                        supervisor.promptContinue();
-                    } else {
-                        //    ****TEST needed
+                    if (response.name.toLowerCase() == list[i].department_name.toLowerCase()) {
+                        inStock = true;
                     }
-                } var query = connection.query(
-                    "INSERT INTO departments SET ?",
-                    {
-                        department_name: response.name,
-                        over_head_costs: response.overhead,
-                    },
-                    function (err, res) {
-                        console.log("Department Registered");
-                        // Call updateProduct AFTER the INSERT completes
-                        supervisor.promptContinue();
-                    }
-                );
+                }
+                if (inStock) {
+                    console.log("deparmtent is already in stock. Please use Add to Inventory option to add");
+                    supervisor.promptContinue();
+                } else {
+                    var query = connection.query(
+                        "INSERT INTO departments SET ?",
+                        {
+                            department_name: response.name,
+                            over_head_costs: response.overhead,
+                        },
+                        function (err, res) {
+                            console.log("Department Registered");
+                            // call promptContinue function to ask if supervisor wants to do another job
+                            supervisor.promptContinue();
+                        }
+                    );
+                }
             });
     },
+
+     // function that let supervisor to decide whether to do another job or finish the app
     promptContinue: function () {
         inquirer
             .prompt({
